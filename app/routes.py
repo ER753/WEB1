e2728f4ccfa6dbebe7348d582973021cc40c350f
xuziyang <none>
2024-09-26 18:01:14 +0800

    3,Utilize html

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,9 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 def hello():
     return"Hello World!!"
+@app.route('/index')
+def index():
+    return render_template('index.html',title='Home')
